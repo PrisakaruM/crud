9f2cbc0665f437d4091cd100c08ab82d2908ec1a
Mihail <prisakaru19996@gmail.com>
2019-10-24 16:59:43 +0300

    Login design

--- app/src/main/java/com/example/crud/MainActivity.java
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText login;
+    public EditText password;
+    public TextView alert;
+    public Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        password = findViewById(R.id.password);
+        alert = findViewById(R.id.alert);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alert.setAlpha(1);
+                alert.setText("login: "
+                              .concat(login.getText().toString())
+                              .concat("\nPassword: ")
+                              .concat(password.getText().toString()));
+
+                submit.setAlpha(0);
+            }
+        });
     }
 }
